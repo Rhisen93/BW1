0b70312fe18105223dafe15730d2bf92441a0a57
Davel-debug <darioblzn@gmail.com>
2025-06-12 18:31:38 +0200

    sistemato problema esplosioni

---
+++ Assets/Scripts/Effects/boom.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class ExplosionEffect : MonoBehaviour
+{
+    public void OnExplosionAnimationStart()
+    {
+        Debug.Log("Animazione esplosione partita!");
+    }
+}
